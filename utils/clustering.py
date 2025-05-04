fe41c97e88494ba34e5f0ed2fc727c5cbcf18048
MANNATMITTAL <124866885+MANNATMITTAL@users.noreply.github.com>
2025-05-04 22:38:32 +0530

    Delete utils directory

--- utils/clustering.py
+++
@@ -1,6 +0,0 @@
-from sklearn.cluster import KMeans
-
-def get_kmeans_clusters(features, n_clusters=10):
-    model = KMeans(n_clusters=n_clusters, random_state=42)
-    labels = model.fit_predict(features)
-    return model, labels
