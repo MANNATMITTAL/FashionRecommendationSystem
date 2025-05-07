fd38b2eafd20a86c41d9a13c7078c3711f3a4a16
MANNATMITTAL <124866885+MANNATMITTAL@users.noreply.github.com>
2025-05-08 00:58:40 +0530

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,6 +1,10 @@
-import cv2
+from PIL import Image
+import numpy as np
 
 def preprocess_image(img):
-    img = cv2.resize(img, (224, 224))
-    img = img / 255.0
+    """
+    Resize the input PIL image to 224x224 and normalize pixel values to [0,1].
+    """
+    img = img.resize((224, 224))
+    img = np.array(img).astype(np.float32) / 255.0
     return img
